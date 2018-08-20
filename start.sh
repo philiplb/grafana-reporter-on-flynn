8e265ee2e290039eaa914fd29e60c4b48e887194
Philip Lehmann-Boehm <philip@philiplb.de>
2018-08-20 14:55:00 +0200

    added the pdflatex location to the path

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -sL "https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh" | sh
+export PATH="$PAH:/app/.TinyTeX/bin/x86_64-linux/"
 ./grafana-reporter -ip "$IP" -port ":$PORT" -proto "$PROTO"
